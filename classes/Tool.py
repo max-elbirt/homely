66f058b625a548a0ae58fbbc13103a815749d522
max-elbirt <117280047+max-elbirt@users.noreply.github.com>
2023-02-13 13:06:52 +0200

    image update capabilities

--- classes/Tool.py
+++ classes/Tool.py
@@ -1,10 +1,11 @@
 class Tool:
-    def __init__(self, user_id, name, type_of = None, category = None, availability = False):
+    def __init__(self, user_id, name, type_of = None, category = None, availability = False, image_ref = None):
         self.user_id = user_id
         self.name = name
         self.type_of = type_of
         self.category = category
         self.availability = availability
+        self.image_ref = image_ref
 
     def get_availability(self):
         return self.availability
--- homelyDB_API.py
+++ homelyDB_API.py
@@ -1,12 +1,27 @@
 import pymongo
+import gridfs
 from classes import Tool
-import User
+from classes import User
+from telegram import Update
+from telegram.ext import Updater
 
 my_client = pymongo.MongoClient("mongodb://localhost:27017")
 
 homely_DB = my_client['homely']
 user_cl = homely_DB['users']
 tool_cl = homely_DB['tools']
+image_cl = homely_DB['images']
+
+fs = gridfs.GridFS(homely_DB)
+
+def store_image_to_user(update: Update, tool: Tool):
+    image = update.message.photo[-1]
+    image_data = image.get_file()
+
+    image_ref = fs.put(image_data, filename=f'{tool.name}{tool.user_id}.jpg')
+
+    tool_cl.update_one({"user_id": tool.user_id}, {"image_ref":image_ref})
+
 
 def add_tool(tool: Tool, user: User):
     tool_cl.insert_one(tool.__dict__)
@@ -24,5 +39,6 @@ def borrow_tool(tool: Tool):
 
 def return_tool(tool: Tool):
     tool_cl[tool].set_availability(True)
+
 
 
